f02d07a061979fe8e45f69ca7fac411b8ecf409c
Ricardo_Mauricio_Delgado_Rivera <rmdr2001@gmail.com>
2024-04-25 20:55:57 -0500

    objeto en app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 'use stric';
 
+function getRandomInt(min, max){
+    return Math.random() * (max - min) + min;
+}
+
 const seatle = {
     locationName: 'Seatle',
-    minCostumerPerHour: 23,
-    maxCostumerPerHour: 65,
-    averageCostumerPerHour:6.3,
+    minCustomerPerHour: 23,
+    maxCustomerPerHour: 65,
+    averageCustomerPerHour:6.3,
     cookiesEachHour:[],
-    
+
+    estimateSales: function() {
+        const cookiesEachHour = [];
+        for (let i = 6; i <=19; i++){
+            const customer = getRandomInt(this.minCustomerPerHour, this.maxCustomerPerHour);
+            const cookiesSold = Math.round(customer * this.averageCustomerPerHour);
+            cookiesEachHour.push(`${i < 12 ? i + 'am' : (i-12) + 'pm'}: ${cookiesSold} galletas`)
+        }
+        return cookiesEachHour;
+    },
     estimate: function() {
         this.cookiesEachHour = this.estimateSales(this);
     }
