afa67b8fb6594459e64e51e54c5c82f3b0606994
Ricardo_Mauricio_Delgado_Rivera <rmdr2001@gmail.com>
2024-04-25 23:51:48 -0500

    Lista de ventas de Seattle

--- js/app.js
+++ js/app.js
@@ -1,31 +1,41 @@
 'use stric';
 
-function getRandomInt(min, max){
-    return Math.random() * (max - min) + min;
-}
-
-const seatle = {
-    locationName: 'Seatle',
-    minCustomerPerHour: 23,
-    maxCustomerPerHour: 65,
-    averageCustomerPerHour:6.3,
-    cookiesEachHour:[],
-
-    estimateSales: function() {
-        const cookiesEachHour = [];
-        for (let i = 6; i <=19; i++){
-            const customer = getRandomInt(this.minCustomerPerHour, this.maxCustomerPerHour);
-            const cookiesSold = Math.round(customer * this.averageCustomerPerHour);
-            cookiesEachHour.push(`${i < 12 ? i + 'am' : (i-12) + 'pm'}: ${cookiesSold} galletas`)
-        }
-        return cookiesEachHour;
-    },
-    estimate: function() {
-        this.cookiesEachHour = this.estimateSales(this);
+document.addEventListener('DOMContentLoaded', function() {
+    
+    function getRandomInt(min, max){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
-}
 
-seatle.estimate();
+    const seattle = {
+        locationName: 'Seattle',
+        minCustomerPerHour: 23,
+        maxCustomerPerHour: 65,
+        averageCustomerPerHour:6.3,
+        cookiesEachHour:[],
+        totalCookies: 0,
 
-const salesSeatle = document.getElementById('salesSeatle');
-const salesDataSeatle = seatle.cookiesEachHour
+        estimateSales: function() {
+            const cookiesEachHour = [];
+            for (let i = 6; i <=19; i++){
+                const customers = getRandomInt(this.minCustomerPerHour, this.maxCustomerPerHour);
+                const cookiesSold = Math.round(customers * this.averageCustomerPerHour);
+                cookiesEachHour.push(`${i <= 12 ? i + 'am' : (i-12) + 'pm'}: ${cookiesSold} galletas`)
+                this.totalCookies += cookiesSold;
+            }
+            this.cookiesEachHour = cookiesEachHour;
+        }
+    }
+
+    seattle.estimateSales();
+
+    const salesSeattle = document.getElementById('salesSeattle');
+
+    for (let i = 0; i < seattle.cookiesEachHour.length; i++) {
+        const item = seattle.cookiesEachHour[i];
+        const listItem = document.createElement('li');
+        listItem.textContent = item;
+        salesSeattle.appendChild(listItem);
+    }
+});
