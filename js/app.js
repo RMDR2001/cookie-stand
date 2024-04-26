a3c804d306b48e3537844ad1ddfc0bced9854469
Ricardo_Mauricio_Delgado_Rivera <rmdr2001@gmail.com>
2024-04-25 20:10:29 -0500

    1er objeto en app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 'use stric';
 
+const seatle = {
+    locationName: 'Seatle',
+    minCostumerPerHour: 23,
+    maxCostumerPerHour: 65,
+    averageCostumerPerHour:6.3,
+    cookiesEachHour:[],
+    
+    estimate: function() {
+        this.cookiesEachHour = this.estimateSales(this);
+    }
+}
